360f8bf6d9764a3f20f816b92c4d3aca30848bd4
clement <cleman91@gmail.com>
2019-10-14 09:20:06 +0200

    c

--- fonctions.c
+++ fonctions.c
@@ -25,46 +25,8 @@ int compare(int* a, int *b, int n){
 	printf("Comparison OK\n\n");
 	return -1;	
 }
-void merge (int* tab, int* tmp, int left, int mid, int right, int* cnt){
-    int indexTab1 = left , indexTab2 = mid;
-    int indextmp = left;
-    int nbModify=0;
-
-    /*
-     * decompose a main table with index right mid and left mid  is the 1st element of right table
-     * compare each element in a logic order, between the two tab , when one is higher it's placed in the temp table and is counter of the
-     * of this table is incremented
-     */
-    while (indexTab1 < mid && indexTab2 <=right)
-    {
-        if(tab[indexTab1]< tab[indexTab2])
-        {
-            tmp[indextmp] = tab[indexTab1];
-            indexTab1++;
-
-        }
-        else
-        {
-            tmp[indextmp] = tab[indexTab2];
-            indexTab2++;
-        }
-        nbModify++;
-        indextmp++;
-    }
 
 
-    while (indexTab1 < mid)//  if it have element in the right table  copy them at the end of the tmp table
-    {
-        tmp[indextmp++] = tab[indexTab1++];
-        nbModify++;
-    }
-    //copy the tmp table into the main table
-    for(indextmp = left; indextmp  < nbModify+left ; indextmp ++ )
-    {
-        tab[indextmp] = tmp[indextmp ];
-        (*cnt) ++ ;
-    }
-}
 
 void swap(int *a, int *b) {
     int c;
@@ -76,14 +38,19 @@ void swap(int *a, int *b) {
 
 int bulle(int *tab, int n) {
     int o = 0, i, l;
+    int trie =0;
     l = n;
     while (l > 0) {
         for (i = 0; i < l-1; i++) {
             if (tab[i] > tab[i + 1]) {
                 swap(&tab[i], &tab[i + 1]);
+                trie = 1;
                 o++;
             }
         }
+        if(!trie) //Si pas de swap pendant un tour de boucle alors le tableau est trié.
+            return(o);
+        trie = 0;
         l--;
     }
     return (o);
@@ -133,5 +100,42 @@ void mergeSort (int* tab,int* tmp, int left, int right,int* cnt){
         mergeSort(tab, tmp, mid+1, right, cnt);
 
         merge(tab, tmp, left, mid+1, right, cnt);
+    }
+}
+void merge (int* tab, int* tmp, int left, int mid, int right, int* cnt){
+    int indexTab1 = left , indexTab2 = mid;
+    int indextmp = left;
+    int nbModify=0;
+    /*
+     * decompose a main table with index right mid and left mid  is the 1st element of right table
+     * compare each element in a logic order, between the two tab , when one is higher it's placed in the temp table and is counter of the
+     * of this table is incremented
+     */
+    while (indexTab1 < mid && indexTab2 <= right)
+    {
+        if(tab[indexTab1] < tab[indexTab2])
+        {
+            tmp[indextmp] = tab[indexTab1];
+            indexTab1++;
+
+        }
+        else
+        {
+            tmp[indextmp] = tab[indexTab2];
+            indexTab2++;
+        }
+        nbModify++;
+        indextmp++;
+    }
+    while (indexTab1 < mid)//  if it have element in the right table  copy them at the end of the tmp table
+    {
+        tmp[indextmp++] = tab[indexTab1++];
+        nbModify++;
+    }
+    //copy the tmp table into the main table
+    for(indextmp = left; indextmp  < nbModify+left ; indextmp ++ )
+    {
+        tab[indextmp] = tmp[indextmp ];
+        (*cnt) ++ ;
     }
 }
