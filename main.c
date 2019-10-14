c9f5f5ae667ace09d103edd9d33b220dc2a05e13
clement <cleman91@gmail.com>
2019-10-14 09:10:38 +0200

    All algorithme work and are merged

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
 	int tab1[] = {14,15,92,65,35,89,79,32,38,46,26,43,38,32,79,50,28,84,19,71,69,39,93,75,10,58,20,97,49,44,59,23,07,81,64,06,28,62,8,99,86,28,03,48,25,34,21,17,06,79};
 	int tab2[] = {99,97,93,92,89,86,84,81,79,79,79,75,71,69,65,64,62,59,58,50,49,48,46,44,43,39,38,38,35,34,32,32,28,28,28,26,25,23,21,20,19,17,15,14,10,8,7,6,6,3};
 	int tab3[] = {3,6,6,7,8,10,14,15,17,19,81,21,23,25,26,28,28,28,32,32,34,35,38,38,39,43,44,46,48,49,50,58,59,62,64,65,69,71,75,79,79,79,20,84,86,89,92,93,97,99};
-	
+	int conter= 0;
 	int n = sizeof(ref)/sizeof(int);
 	///////Bubble Sort///////////////////
     /**
@@ -24,6 +24,9 @@ int main(){
     printf("Bubble Sort tab ref: operation counter = %d \t",bulle(ref,n)); //// 0 operation
     compare(ref,ref,n);
     /////////////////////////////
+     int insertab1[] = {14,15,92,65,35,89,79,32,38,46,26,43,38,32,79,50,28,84,19,71,69,39,93,75,10,58,20,97,49,44,59,23,07,81,64,06,28,62,8,99,86,28,03,48,25,34,21,17,06,79};
+     int insertab2 []= {99,97,93,92,89,86,84,81,79,79,79,75,71,69,65,64,62,59,58,50,49,48,46,44,43,39,38,38,35,34,32,32,28,28,28,26,25,23,21,20,19,17,15,14,10,8,7,6,6,3};
+     int insertab3 []= {3,6,6,7,8,10,14,15,17,19,81,21,23,25,26,28,28,28,32,32,34,35,38,38,39,43,44,46,48,49,50,58,59,62,64,65,69,71,75,79,79,79,20,84,86,89,92,93,97,99};
 
     ///////////Insertion Sort////////////
     /**
@@ -31,15 +34,18 @@ int main(){
      *
      * La complexite spacial  est de n, l'algorithme est stable en taille
      */
-    printf("Insertion Sort tab1 : Operation Counter =  %d \t",insertionSort(tab1,n)); //// 691 operation
-    compare(tab1,ref,n);
-    printf("Insertion Sort tab2 : Operation Counter =  %d \t",insertionSort(tab2,n));////1225 operation
-    compare(tab2,ref,n);
-    printf("Insertion Sort tab3 : Operation Counter =  %d \t",insertionSort(tab3,n)); ////72 operation
-    compare(tab3,ref,n);
+    printf("Insertion Sort tab1 : Operation Counter =  %d \t",insertionSort(insertab1,n)); //// 691 operation
+    compare(insertab1,ref,n);
+    printf("Insertion Sort tab2 : Operation Counter =  %d \t",insertionSort(insertab2,n));////1225 operation
+    compare(insertab2,ref,n);
+    printf("Insertion Sort tab3 : Operation Counter =  %d \t",insertionSort(insertab3,n)); ////72 operation
+    compare(insertab3,ref,n);
     printf("Insertion Sort ref : Operation Counter =  %d \t",insertionSort(ref,n)); //// 0 opération
     compare(ref,ref,n);
     //////////////////////////////////////
+    int mergetab1 []= {14,15,92,65,35,89,79,32,38,46,26,43,38,32,79,50,28,84,19,71,69,39,93,75,10,58,20,97,49,44,59,23,07,81,64,06,28,62,8,99,86,28,03,48,25,34,21,17,06,79};
+    int mergetab2 []= {99,97,93,92,89,86,84,81,79,79,79,75,71,69,65,64,62,59,58,50,49,48,46,44,43,39,38,38,35,34,32,32,28,28,28,26,25,23,21,20,19,17,15,14,10,8,7,6,6,3};
+    int mergetab3 []= {3,6,6,7,8,10,14,15,17,19,81,21,23,25,26,28,28,28,32,32,34,35,38,38,39,43,44,46,48,49,50,58,59,62,64,65,69,71,75,79,79,79,20,84,86,89,92,93,97,99};
     ///////////////////////////mergeSort///////////////////
     /**
      * la complexité temporel de cet algorithme est en log10(n) O(lg(n))
@@ -50,17 +56,17 @@ int main(){
     tmp =malloc(sizeof(ref));
     if( tmp== NULL)
         return  1;
-    mergeSort(tab1,tmp,0,n-1,&conter);
+    mergeSort(mergetab1,tmp,0,n-1,&conter);
     printf("MergeSort tab1 : counter = %d \t ",conter); //////counter = 259
-    compare(tab1,ref,n);
+    compare(mergetab1,ref,n);
     conter = 0;
-    mergeSort(tab2,tmp,0,n-1,&conter);
+    mergeSort(mergetab2,tmp,0,n-1,&conter);
     printf("MergeSort tab2 : counter = %d \t ",conter); ////counter = 286
-    compare(tab2,ref,n);
+    compare(mergetab2,ref,n);
     conter = 0;
-    mergeSort(tab3,tmp,0,n-1,&conter);
+    mergeSort(mergetab3,tmp,0,n-1,&conter);
     printf("MergeSort tab3 : counter = %d \t ",conter);////counter = 196
-    compare(tab3,ref,n);
+    compare(mergetab3,ref,n);
     conter = 0;
     printf("MergeSort ref : conter = %d \t ",conter);////counter = 0
     compare(ref,ref,n);
