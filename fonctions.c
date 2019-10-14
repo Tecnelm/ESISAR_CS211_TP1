beaa496297a63f0a8f4e7541ec20cb15d59114ab
Tecnelm <34011127+Tecnelm@users.noreply.github.com>
2019-10-14 11:03:17 +0200

    Update fonctions.c

--- fonctions.c
+++ fonctions.c
@@ -25,44 +25,42 @@ int compare(int* a, int *b, int n){
 	printf("Comparison OK\n\n");
 	return -1;	
 }
-void merge (int* tab, int* tmp, int left, int mid, int right, int* cnt){
-    int indexTab1 = left , indexTab2 = mid;
+void merge(int *tab, int *tmp, int left, int mid, int right, int *cnt) {
+    int indexTab1 = left, indexTab2 = mid;
     int indextmp = left;
-    int nbModify=0;
-
+    int nbModify = 0;
     /*
      * decompose a main table with index right mid and left mid  is the 1st element of right table
      * compare each element in a logic order, between the two tab , when one is higher it's placed in the temp table and is counter of the
      * of this table is incremented
      */
-    while (indexTab1 < mid && indexTab2 <=right)
-    {
-        if(tab[indexTab1]< tab[indexTab2])
-        {
+    if (tab[mid - 1] < tab[mid]) { // compare the last element of the first table and the first of the second table , as they are already sort we can abort the merge turn
+        (*cnt)++;
+        return;
+    }
+    while (indexTab1 < mid && indexTab2 <= right) {
+        if (tab[indexTab1] < tab[indexTab2]) {
             tmp[indextmp] = tab[indexTab1];
             indexTab1++;
-
-        }
-        else
-        {
+        } else {
             tmp[indextmp] = tab[indexTab2];
             indexTab2++;
         }
+        (*cnt)++;
         nbModify++;
         indextmp++;
     }
-
-
     while (indexTab1 < mid)//  if it have element in the right table  copy them at the end of the tmp table
     {
         tmp[indextmp++] = tab[indexTab1++];
+        (*cnt)++;
         nbModify++;
     }
+
     //copy the tmp table into the main table
-    for(indextmp = left; indextmp  < nbModify+left ; indextmp ++ )
-    {
-        tab[indextmp] = tmp[indextmp ];
-        (*cnt) ++ ;
+    for (indextmp = left; indextmp < nbModify + left; indextmp++) {
+        tab[indextmp] = tmp[indextmp];
+        (*cnt)++;
     }
 }
 
